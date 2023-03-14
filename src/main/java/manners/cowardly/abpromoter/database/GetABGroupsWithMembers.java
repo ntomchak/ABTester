a1574b7f3cb399fbf4dc51280e0b2f38d9e4042d
ntomchak <55006670+ntomchak@users.noreply.github.com>
2023-03-14 04:03:06 -1000

    Create GetABGroupsWithMembers.java

---
+++ src/main/java/manners/cowardly/abpromoter/database/GetABGroupsWithMembers.java
@@ -0,0 +1,39 @@
+package manners.cowardly.abpromoter.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import manners.cowardly.abpromoter.ABPromoter;
+import manners.cowardly.abpromoter.database.connect.ConnectionPool;
+
+public class GetABGroupsWithMembers {
+    private ConnectionPool pool;
+
+    public GetABGroupsWithMembers(ConnectionPool pool) {
+        this.pool = pool;
+    }
+
+    // from async or sync during startup/reload
+    public Collection<String> abGroupsWithMembers(String tableName, String usersTableColumnName) {
+        try (Connection c = pool.getConnection()) {
+            Collection<String> groupNames = new ArrayList<String>();
+            PreparedStatement s = c.prepareStatement(
+                    "SELECT name FROM " + tableName + " WHERE EXISTS (SELECT 1 FROM users WHERE users."
+                            + usersTableColumnName + " = " + tableName + ".id)");
+            ResultSet r = s.executeQuery();
+            while (r.next())
+                groupNames.add(r.getString("name"));
+            r.close();
+            s.close();
+            return groupNames;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            ABPromoter.getInstance().getLogger().severe("Unable to get the list of A/B groups in " + tableName);
+            return null;
+        }
+    }
+}
